d33dfe990fb23fdad0ac7f04c4f067720d26e94a
manoj gaikwad <gaikwadm853@gmail.com>
2021-08-21 18:35:37 +0530

    Added: check one line is equal, greater or less than the other line.

--- src/main/java/com/bridgelabz/LineComparison/App.java
+++ src/main/java/com/bridgelabz/LineComparison/App.java
@@ -7,8 +7,9 @@ public class App
     public static void main( String[] args )
     {
     	Scanner scan = new Scanner(System.in);
-    	int x1, x2, y1, y2, m1, m2, n1, n2;
+    	int x1, x2, y1, y2, m1, m2, n1, n2, comp;
     	double length1, length2;
+    	String line1, line2;
         System.out.println( "Hello welcome to line comparison computation program on master branch" );
         System.out.print("Enter value of (x1 y1) (x2 y2) : ");
         x1 = scan.nextInt();
@@ -26,11 +27,19 @@ public class App
         System.out.println("Length of line 1 : " +length1);
         System.out.println("Length of line 2 : " +length2);
         
-        if (length1 == length2) {
-			System.out.println("lines are equal");
+        line1 =  Double.toString(length1);
+        line2 =  Double.toString(length2);
+        
+        comp = line1.compareTo(line2);
+        
+        if(comp == 0) {
+        	System.out.println("Line 1 is equals to line 2");			        	
+        }
+        else if (length1 > length2) {
+			System.out.println("Line 1 is greater then line2");
 		}
 		else {
-			System.out.println("lines are not equal");			
+			System.out.println("Line 2 is greater then line 1");
 		}
 
     }
